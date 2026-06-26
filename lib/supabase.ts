4c40fac0726dc43050b403cb88ae927f7e2ffa04
Koterie Dev <koterie@example.com>
2026-06-26 16:43:52 -0300

    actualización

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("¡Error crítico! Las variables de Supabase no están llegando a Vercel.")
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
